66183589e8b6303267d0e2456f073b44b5a3542d
Maninderpal Singh <sin14329@sheridancollege.ca>
2024-02-20 12:10:41 -0500

    Created a new class

---
+++ src/midtermexam_versiona_extensioncode/maninderpal_singh.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author manny
+ */
+public class maninderpal_singh {
+    //I have created this class
+}
